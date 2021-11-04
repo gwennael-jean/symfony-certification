f46318c2d886dbf7f281d343b1a8b185453bf417
gwennael <gwennael.jean@digiwin.fr>
2021-11-04 12:20:22 +0100

    chore(asset): activation de PostCSS

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,9 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+    // enables PostCSS support
+    .enablePostCssLoader()
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
