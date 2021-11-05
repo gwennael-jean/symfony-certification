78674eda8b3a6d32574c57d56ce9992439a10a3a
gwennael <gwennael.jean@digiwin.fr>
2021-11-05 12:02:31 +0100

    chore(asset): ajout de couleur supplémentaire

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: theme => ({
+        'primary': '#9b59b6',
+        'secondary': '#95a5a6',
+        'success': '#2ecc71',
+        'error': '#e74c3c',
+        'infos': '#3498db',
+        'warning': '#e67e22',
+      })
+    },
   },
   variants: {
     extend: {},
