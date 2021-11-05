ac79bac34d681e7586c2fa86f12e1f0ed3cd0f60
gwennael <gwennael.jean@digiwin.fr>
2021-11-05 13:33:38 +0100

    chore(inscription): déplacement du FormType dans le dossiser Security

--- src/Controller/Security/RegistrationController.php
+++ src/Controller/Security/RegistrationController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Security;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Form\Security\RegistrationFormType;
 use App\Repository\UserRepository;
 use App\Security\EmailVerifier;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
