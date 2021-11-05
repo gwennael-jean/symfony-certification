850a35ae9c3ac67468f3e87545170211110d5407
gwennael <gwennael.jean@digiwin.fr>
2021-11-05 18:59:32 +0100

    feat(quizz): ajout du domaine dans la méthode generate()

--- src/Service/UserQuizzGenerator.php
+++ src/Service/UserQuizzGenerator.php
@@ -2,11 +2,19 @@
 
 namespace App\Service;
 
+use App\Entity\Quizz\Domain;
 use App\Entity\Quizz\Quizz;
+use App\Repository\Quizz\QuestionRepository;
 
 class UserQuizzGenerator implements UserQuizzGeneratorInterface
 {
-    public function generate()
+    public function __construct(
+        private QuestionRepository $questionRepository
+    )
+    {
+    }
+
+    public function generate(Domain $domain)
     {
         // TODO: Implement generate() method.
     }
--- src/Service/UserQuizzGeneratorInterface.php
+++ src/Service/UserQuizzGeneratorInterface.php
@@ -2,11 +2,12 @@
 
 namespace App\Service;
 
+use App\Entity\Quizz\Domain;
 use App\Entity\Quizz\Quizz;
 
 interface UserQuizzGeneratorInterface
 {
-    public function generate();
+    public function generate(Domain $domain);
 
     public function generateByQuizz(Quizz $quizz);
 }
