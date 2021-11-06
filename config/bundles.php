e3a2d4f0d81ac80e4e6e559d43e859e306f6d749
gwennael <gwennael.jean@digiwin.fr>
2021-11-06 11:16:06 +0100

    feat(app): installation de symfony/debug-pack

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,7 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
