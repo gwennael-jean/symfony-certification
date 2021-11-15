135328d2bbf5467667c93ad91ac57b6ef341fd5f
gwennael <gwennael.jean@digiwin.fr>
2021-11-15 22:07:35 +0100

    feat(app): utilisation du doctrine param converter

--- src/Controller/Quizz/UserQuizzController.php
+++ src/Controller/Quizz/UserQuizzController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller\Quizz;
 
+use App\Entity\Quizz\Domain;
 use App\Entity\Quizz\UserQuizz;
 use App\Form\UserQuizzType;
-use App\Repository\Quizz\DomainRepository;
-use App\Repository\Quizz\UserQuizzRepository;
 use App\Service\UserQuizzGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,17 +15,13 @@ class UserQuizzController extends AbstractController
 {
     public function __construct(
         private UserQuizzGeneratorInterface $userQuizzGenerator,
-        private DomainRepository            $domainRepository,
-        private UserQuizzRepository         $userQuizzRepository,
     )
     {
     }
 
     #[Route('/user-quizz/{id}', name: 'userquizz_index')]
-    public function index(Request $request, int $id): Response
+    public function index(Request $request, UserQuizz $userQuizz): Response
     {
-        $userQuizz = $this->userQuizzRepository->find($id);
-
         $form = $this->createForm(UserQuizzType::class, $userQuizz);
 
         $form->handleRequest($request);
@@ -46,10 +41,8 @@ class UserQuizzController extends AbstractController
     }
 
     #[Route('/user-quizz/domain/{id}/generate', name: 'userquizz_generate_by_domain')]
-    public function generateByDomain(int $id): Response
+    public function generateByDomain(Domain $domain): Response
     {
-        $domain = $this->domainRepository->find($id);
-
         $userQuizz = ($this->userQuizzGenerator->generateByDomain($domain))
             ->setUser($this->getUser());
 
@@ -62,10 +55,8 @@ class UserQuizzController extends AbstractController
     }
 
     #[Route('/user-quizz/{id}/update', name: 'userquizz_update', methods: ['POST'])]
-    public function update(Request $request, int $id): Response
+    public function update(Request $request, UserQuizz $userQuizz): Response
     {
-        $userQuizz = $this->userQuizzRepository->find($id);
-
         $this->getDoctrine()->getManager()->persist($userQuizz);
         $this->getDoctrine()->getManager()->flush();
 
